473695c9089c66fa498e153ccdb9e0770bf47475
Crispino <gabriel.crispino@hotmail.com>
2019-03-24 23:16:29 -0300

    Create parallel structure draft

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
+#include <cmath>
 #include <fstream>
 #include <iostream>
 #include <regex>
 #include <string>
 #include <vector>
 #include <dirent.h>
+#include <pthread.h>
 #include <DirInfo.hpp>
+
+struct thread_param
+{
+	std::vector<std::string> *arrRef;
+	std::regex regex;
+	int start, end, n;
+};
 
 int getMaxThreads(char **argv)
 {
@@ -48,12 +57,14 @@ std::vector<unsigned> searchRegexFile(std::regex expr, std::string filePath)
 	std::ifstream file(filePath);
 	std::vector<unsigned> matches;
 
+	std::cout << "File path: " << filePath << std::endl;
 	if (file)
 	{
 		std::string line;
 		unsigned lineI = 0;
 		while (getline(file, line))
 		{
+			// std::cout << "line: " << line << std::endl;
 			std::smatch m;
 			if (std::regex_search(line, m, expr))
 			{
@@ -67,6 +78,29 @@ std::vector<unsigned> searchRegexFile(std::regex expr, std::string filePath)
 	return matches;
 }
 
+void *searchRegexFiles(void *args)
+{
+	struct thread_param *params = (struct thread_param *)args;
+
+	std::cout << "Iniciando searchRegexFiles: " << params->start << ',' << params->end << std::endl;
+	std::vector<std::string> *vec = params->arrRef;
+	std::cout << "  Arquivos: " << std::endl;
+	for (int i = params->start; i <= params->end && i < params->n; ++i)
+	{
+
+		std::cout << "    " << (*vec)[i] << std::endl;
+	}
+	for (int i = params->start; i <= params->end && i < params->n; ++i)
+	{
+
+		searchRegexFile(params->regex, (*vec)[i]);
+	}
+	std::cout << "Finalizar searchRegexFiles: " << params->start << ',' << params->end << std::endl;
+
+	return NULL;
+	//------------------------------------------------------------------------------
+}
+
 int main(int argc, char **argv)
 {
 	if (argc < 4)
@@ -78,6 +112,7 @@ int main(int argc, char **argv)
 
 	int maxThreads = getMaxThreads(argv);
 	std::string regexStr(argv[2]), searchPath(argv[3]);
+	std::vector<struct thread_param> params(maxThreads);
 
 	std::vector<pthread_t> threads(maxThreads);
 	std::regex regex(regexStr);
@@ -89,10 +124,23 @@ int main(int argc, char **argv)
 	std::cout << "Search path: " << searchPath << std::endl;
 	std::vector<std::string> res = dirInfo.getFileList();
 
-	std::vector<unsigned> lineMatches = searchRegexFile(regex, res[0]);
-
-	for (unsigned matchLine : lineMatches)
+	for (int i = 0; i < maxThreads; ++i)
 	{
-		std::cout << res[0] << ':' << matchLine << std::endl;
+		unsigned n = res.size();
+		int d = ceil((float)n / maxThreads);
+		params[i].arrRef = &res;
+		params[i].start = d * i;
+		params[i].end = d * i + d - 1;
+		params[i].regex = regex;
+		params[i].n = n;
+
+		pthread_create(&threads[i], NULL, searchRegexFiles, &params[i]);
 	}
+
+	for (int i = 0; i < maxThreads; ++i)
+	{
+		pthread_join(threads[i], NULL);
+	}
+
+	return 0;
 }
